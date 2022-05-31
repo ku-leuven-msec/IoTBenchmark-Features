10a566efc484b5bcdea1313b9c212af9c99511c9
victorGoeman <victorgoeman@hotmail.com>
2022-05-31 11:07:06 +0200

    scripts tests

--- Features/Misconfigurations/ssh/addBadSSHConfig.sh
+++ Features/Misconfigurations/ssh/addBadSSHConfig.sh
@@ -1,7 +1,8 @@
-wget https://raw.githubusercontent.com/DairodR/Benchmark-Repo-Features/main/Features/Misconfigurations/ssh/addBadSSHConfig.sh -O sshd_config 
+wget "${MisconfigurationsURL}ssh/sshd_config" -O sshd_config 
 #TODO from github
 #TODO work with symbolic link instead of 2 different files
 # If the vulnerable sshd is also on the device
+rm /etc/ssh/sshd_config
 mv sshd_config /etc/ssh/sshd_config
 mkdir -p /usr/local/etc/ssh/
 ln /etc/ssh/sshd_config /usr/local/etc/ssh/
