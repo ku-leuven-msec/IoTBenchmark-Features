490fcedfd124d758d1213a2023656038921a462c
victorGoeman <victorgoeman@hotmail.com>
2022-06-02 16:18:37 +0200

    All scripts working

--- Features/Resources/certificates/addCertificates.sh
+++ Features/Resources/certificates/addCertificates.sh
@@ -1,42 +1,42 @@
-wget "${ResourcesURL}certificates/client.cnf" _O client.cnf
-wget "${ResourcesURL}certificates/root.cnf" _O root.cnf
-wget "${ResourcesURL}certificates/server.cnf" _O server.cnf
+ResourcesURL="https://raw.githubusercontent.com/DairodR/Benchmark-Repo-Features/main/Features/Resources/"
+wget "${ResourcesURL}certificates/client.cnf" -O client.cnf
+wget "${ResourcesURL}certificates/root.cnf" -O root.cnf
+wget "${ResourcesURL}certificates/server.cnf" -O server.cnf
+
+mkdir /tmp/cert
+mkdir /tmp/tmp
 
 #Create selfisgned CA Certificate
-openssl req -x509 -new -keyout ./cert/root.key -out ./cert/root.cer -config root.cnf
+openssl req -x509 -new -keyout /tmp/cert/root.key -out /tmp/cert/root.cer -config root.cnf -passout pass:test
 #Creating client key pair & cert request
-openssl req -nodes -new -keyout ./cert/client.key -out ./tmp/client.csr -config client.cnf
+openssl req -nodes -new -keyout /tmp/cert/client.key -out /tmp/tmp/client.csr -config client.cnf -passout pass:test
 #generate client certifacates
-openssl x509 -req -in ./tmp/client.csr -CA ./cert/root.cer -CAkey ./cert/root.key -out ./cert/client.cer -CAcreateserial -days 365 -extfile client.cnf -extensions x509_ext
+openssl x509 -req -in /tmp/tmp/client.csr -CA /tmp/cert/root.cer -CAkey /tmp/cert/root.key -passin pass:test -out /tmp/cert/client.cer -CAcreateserial -days 365 -extfile client.cnf -extensions x509_ext
 #Ceeate pkc12 keystore for android
-openssl pkcs12 -export -inkey ./cert/client.key  -in ./cert/client.cer -certfile ./cert/root.cer -out ./cert/client.pfx
+openssl pkcs12 -export -inkey /tmp/cert/client.key  -in /tmp/cert/client.cer -certfile /tmp/cert/root.cer -passout pass:test -out /tmp/cert/client.pfx
 
-mkdir -p /etc/credentials
-mv ./root.cer /etc/credentials/
-mv ./clients.pem /etc/credentials/
-
-local ip=`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
-if [ -z "$ip" ]; then
-ip=`ifconfig eth0 2>/dev/null|awk '/inet / {print $2}'|sed 's/addr://'`
-fi
-echo $ip
-IP=$(get_current_ip)
 DNS="DVD"
-
-cd /etc/credentials
 CFG_PATH="/tmp"
-
-mkdir tmp
 NAME="server"
 
-cat "$CFG_PATH/server.cnf" > "./tmp/$NAME.cnf"
-echo "DNS.2 = $DNS">>"./tmp/$NAME.cnf"
-echo "IP.1 = 127.0.0.1" >> "./tmp/$NAME.cnf" 
-echo "IP.2 = $IP" >> "./tmp/$NAME.cnf" 
-
+echo "DNS.2 = $DNS">> s
+server.cnf
+echo "IP.1 = 127.0.0.1" >> server.cnf 
 
 #Create server key pair & certificate request
-openssl req -nodes -new -keyout $NAME.key -out ./tmp/$NAME.csr -config ./tmp/$NAME.cnf
+openssl req -nodes -new -keyout /tmp/server.key -out /tmp/tmp/server.csr -config server.cnf
 #Generate server certificate
-openssl x509 -days 3650 -req -in ./tmp/$NAME.csr -CA "$CFG_PATH/cert/root.cer" -passin pass:test -CAkey "$CFG_PATH/cert/root.key" -CAcreateserial -out $NAME.cer -extfile ./tmp/$NAME.cnf -extensions x509_ext
+openssl x509 -days 3650 -req -in /tmp/tmp/server.csr -CA /tmp/cert/root.cer -passin pass:test -CAkey /tmp/cert/root.key -CAcreateserial -out /tmp/cert/server.cer -extfile server.cnf -extensions x509_ext
 
+mkdir -p /etc/credentials
+
+cp /tmp/cert/root.cer /etc/credentials/clients.pem
+cat /tmp/cert/client.cer >> /etc/credentials/clients.pem
+mv /tmp/cert/root.cer /etc/credentials/
+mv /tmp/cert/server.cer/ /etc/credentials/
+mv /tmp/server.key /etc/credentials/
+
+rm client.cnf
+rm root.cnf
+rm server.cnf
+rm -r /tmp/tmp
