4cc716e806110813fc8e224cb110fca32ad564a1
victorGoeman <victorgoeman@hotmail.com>
2022-06-22 11:25:08 +0200

    ApacheThings

---
+++ Features/Vulnerabilities/Apache/sourceCode/cgi-bin/whoisthis.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print ("Content-type:text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+print("<title>Hello World - First CGI Program</title>")
+print("</head>")
+print("<body>")
+print("</body>")
+print ("</html>")
+
+
