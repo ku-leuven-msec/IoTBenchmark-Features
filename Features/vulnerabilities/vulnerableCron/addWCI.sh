fe333f70af2aba8b568e0504d1e9bb57516b9c52
victorGoeman <victorgoeman@hotmail.com>
2022-11-10 16:48:40 +0100

    extend Sudo and cron
    Add more modularity
    add wildcardinjection, LD PRELOAD, $PATH, ...

--- Features/vulnerabilities/vulnerableCron/addWCI.sh
+++ Features/vulnerabilities/vulnerableCron/addWCI.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-touch /home/manager/checkNetwork.sh
-echo 'ping -c 1 google.com' > /home/manager/checkNetwork.sh
-chown :managerGroup /home/manager/checkNetwork.sh
-chmod 771 /home/manager/checkNetwork.sh
-echo '* * * * * /home/manager/checkNetwork.sh' >> /etc/crontabs/root
-
 apk add tar
 
 mkdir -p /home/client/backups/
