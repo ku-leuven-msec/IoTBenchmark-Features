39becafab8093e366b2b668c1870ba41b9be1020
victorGoeman <victorgoeman@hotmail.com>
2022-12-19 14:41:45 +0100

    Structural changes for evaluation csv

--- Features/vulnerabilities/badSudoRights/env/addENV.sh
+++ Features/vulnerabilities/badSudoRights/env/addENV.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'ping -c 1 google.com' > /home/manager/checkNetwork.sh
-echo '* * * * * /home/client/script.sh' >> /etc/crontabs/root
+echo '* * * * * /home/manager/checkNetwork.sh' >> /etc/crontabs/root
 echo 'manager ALL=(root) SETENV:/home/manager/checkNetwork.sh' >> /etc/sudoers
