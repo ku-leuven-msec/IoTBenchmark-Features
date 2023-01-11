df9ea03c1a3b8ac05e8bde13acd22b2ccc861b50
victorGoeman <victorgoeman@hotmail.com>
2023-01-11 09:41:19 +0100

    vsftp adress fix

--- Features/misconfigurations/vsftp/addBadFTPConfig.sh
+++ Features/misconfigurations/vsftp/addBadFTPConfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget "${MisconfigurationsURL}vsftp/vsftpd.conf" 
+wget "https://raw.githubusercontent.com/DairodR/Benchmark-Repo-Features/develop/Features/misconfigurations/vsftp/vsftpd.conf" 
 apk add vsftpd
 mkdir -p /etc/vsftpd/
 mv vsftpd.conf /etc/vsftpd/vsftpd.conf
