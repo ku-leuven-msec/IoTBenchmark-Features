1a2fcbf7a963c8c0b30ce858129d03fca91a9951
victorGoeman <victorgoeman@hotmail.com>
2023-03-08 10:58:20 +0100

    fixes

--- Features/vulnerabilities/individualCVEs/mosquitto/addMosquitto.sh
+++ Features/vulnerabilities/individualCVEs/mosquitto/addMosquitto.sh
@@ -4,5 +4,5 @@
 # https://security.alpinelinux.org/vuln/CVE-2021-34431
 # https://security.alpinelinux.org/vuln/CVE-2021-41039
 apk del mosquitto
-apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/v3.10/main/ mosquitto=1.6.8-r1
+apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/v3.10/main/ mosquitto=1.6.3-r0
 rc-update add mosquitto default
--- Features/vulnerabilities/individualCVEs/zlib/addZlibCVE.sh
+++ Features/vulnerabilities/individualCVEs/zlib/addZlibCVE.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /tmp
+apk add alpine-sdk
 wget https://github.com/HardySimpson/zlog/archive/refs/tags/1.2.15.tar.gz
 tar -zxvf 1.2.15.tar.gz
 cd zlog-1.2.15
